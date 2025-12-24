7c748ea47245abb6c5a35bd8106762db2e4c92b7
AlexChui <chuiyaunam@gmail.com>
2025-12-24 20:48:11 +0000

    24DEC

--- Platform.py
+++ Platform.py
@@ -1,5 +1,44 @@
+import Quiz
+
+
 def Teacher():
-    print("TeacherPlatform")
+    while True:
+        try:
+            inputpassword = int(input("Enter password or 0 to exit\n"))
+            if inputpassword == 1234:
+                ShowTeacherMenu()
+                TeacherMenuChoice()
+                break
+            elif inputpassword == 0:
+                break
+            else:
+                print("Invalid password")
+        except:
+            print("Invalid password")
+
+
+
+
+def ShowTeacherMenu():
+    print("1. View/ Edit Quiz\n2.Start Quiz\n3. View Score\n0.Quit\n")
+
+def TeacherMenuChoice():
+    try:
+        choice = int(input())
+        match choice:
+            case 1 :
+                Quiz.Edit()
+            case 2:
+                Quiz.Start()
+            case 3:
+                Quiz.ViewScore()
+            case 0:
+                return
+            case _:
+                print("Invalid input")
+    except:
+        print("Invalid input")
+
 
 def Student():
     print("StudentPlatform")
---
+++ Quiz.py
@@ -0,0 +1,27 @@
+def Edit():
+    while True:
+        try:
+            choice = int(input("1.Add Question\n0.Return"))
+            if choice not in [0,1]:
+                print("Invalid input")
+            elif choice == 1:
+                try:
+                    inputQuestion = input("Enter question")
+                    inputOptionA = input("Enter Option A")
+                    inputOptionB = input("Enter Option B")
+                    inputOptionC = input("Enter Option C")
+                    inputOptionD = input("Enter Option D")
+                    inputAnswer = input("Enter the correct answer")
+                    inputQuestionTuple = (inputQuestion,inputOptionA,inputOptionB,inputOptionC,inputOptionD)
+
+                except:
+                    print("Unable to enter question")
+
+        except:
+            print("Invalid input")
+
+def Start():
+    print()
+
+def ViewScore():
+    print()
